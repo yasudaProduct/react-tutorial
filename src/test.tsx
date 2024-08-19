bd2f037b75a70e2005d96a8ec18558a7a56b05d0
yuta <yasu.private.ct@gmail.com>
2024-08-19 14:46:31 +0900

    install Material UI

---
+++ src/test.tsx
@@ -0,0 +1,11 @@
+function Test() {
+    const title: string = "test";
+  
+    return (
+      <div className="Test">
+        <h1>{title}</h1>
+      </div>
+    );
+  }
+  
+  export default Test;
