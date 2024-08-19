f8cf177e5d875b60a9510e1fc96004b81cad4cb4
yuta <yasu.private.ct@gmail.com>
2024-08-19 15:10:41 +0900

    header init

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Button } from '@mui/material';
+import { Button, createTheme, CssBaseline, makeStyles, ThemeProvider } from '@mui/material';
+import Header from './components/Header';
 
 function App() {
   const text: string = 'Hello World';
 
   return (
     <div className="App">
+      <Header />
       <h1>{text}</h1>
       <Button variant="contained">Hello world</Button>
     </div>
